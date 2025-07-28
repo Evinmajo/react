67a4d7c90f42cd5dfdbe2b1eb1db15e57ed87413
wyenfos013 <wyenfos013@gmail.com>
2025-07-28 12:59:04 +0530

    first react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Greetings from './components/greetings'
+
+function App() {
+
+
+
+  return (
+    <>
+      <Greetings name="Evin" age = "22" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greetings = (props) => {
+  return (
+    <div>Hello {props.name} your age is {props.age}</div>
+  )
+}
+
+export default Greetings
